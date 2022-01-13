9686bc823b3373e1558710f2d12cc8e5723bd5e6
jlrpuma <jlrp39@gmail.com>
2022-01-13 14:34:54 -0500

    A ui library was added to the project just to organize a little bit the application

--- src/infraestructure/services/api/expenses/index.js
+++ src/infraestructure/services/api/expenses/index.js
@@ -8,16 +8,52 @@ export default {
             data: [
                 {
                     id: 1,
-                    name: 'Gas'
+                    name: 'Gas',
+                    cost: 5000,
+                    date: '20/01/2019',
+                    description: 'pago de Gas mes X',
+                    category: 'Servicios'
                 },
                 {
                     id: 2,
-                    name: 'Electricidad'
+                    name: 'Electricidad',
+                    cost: 290000,
+                    date: '24/02/2019',
+                    description: 'pago de Electricidad mes X',
+                    category: 'Servicios'
                 }
                 ,
                 {
                     id: 3,
-                    name: 'Internet'
+                    name: 'Comida Mascotas',
+                    cost: 90000,
+                    date: '26/03/2019',
+                    description: 'Comida Conchita',
+                    category: 'Mascotas'
+                },
+                {
+                    id: 4,
+                    name: 'Gas',
+                    cost: 5000,
+                    date: '20/01/2019',
+                    description: 'pago de Gas mes X',
+                    category: 'Servicios'
+                },
+                {
+                    id: 5,
+                    name: 'Electricidad',
+                    cost: 290000,
+                    date: '24/02/2019',
+                    description: 'pago de Electricidad mes X',
+                    category: 'Servicios'
+                },
+                {
+                    id: 6,
+                    name: 'Comida Mascotas',
+                    cost: 90000,
+                    date: '26/03/2019',
+                    description: 'Comida Conchita',
+                    category: 'Mascotas'
                 }
             ]
         }
--- src/views/App.js
+++ src/views/App.js
@@ -1,45 +1,126 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { getExpenses } from '../application/selectors/expenses';
 import { getLoading } from '../application/selectors/loading'
 import { pageLoaded } from '../application/actions/loading';
 import { putExpense } from '../application/actions/expenses';
+import { Grid, List, makeStyles, ListItem, Card, CardContent, Typography, CardHeader, TextField } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import NumberFormat from 'react-number-format';
 
 const modifiedExpenses = (expenses) => {
   let clonedExpenses = [...expenses]
-  clonedExpenses.push({id: 2, name: 'energia', anything: 0})
+  clonedExpenses.push({ id: 2, name: 'energia', anything: 0 })
   return clonedExpenses
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  card: {
+    minWidth: '100%'
+  },
+  cardContent: {
+    display: 'inline-grid'
+  },
+  list: {
+    maxHeight: 500,
+    overflow: 'auto'
+  }
+}));
+
 export default () => {
 
+  const classes = useStyles();
   const dispatch = useDispatch();
-
   const expenses = useSelector(getExpenses)
   const loading = useSelector(getLoading);
 
+  const [search, setSearch] = useState('');
+
   useEffect(() => {
     dispatch(pageLoaded)
-  }, [dispatch])  
- 
+  }, [dispatch])
+
+  useEffect(() => {
+    console.log(search)
+  }, [search]) 
+
+  const filter = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <h1> Expenses </h1>
-      <button onClick={() => dispatch(putExpense(modifiedExpenses(expenses)))}>add Expense</button>
-      {loading ? 
-      'Loading expenses...' : 
-      (<ul>
-        {expenses.map(expense => (
-          <li
-            key={expense.id} 
-            onClick={() => dispatch(putExpense(modifiedExpenses(expenses)))}
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h1> Expenses </h1>
+        </Grid>
+        <Grid item xs={3}>
+          <h3> Some options</h3>
+        </Grid>
+        <Grid item xs={8}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="stretch"
           >
-            {expense.name}
-          </li>
-        ))}
-      </ul>)
-      }
+            <TextField label="Search" variant="outlined" type="search" value={search} onChange={filter}/>
+            {loading ?
+            (<>
+              <Skeleton height={100} width={'100%'} />
+              <Skeleton animation={false} height={100} width={'100%'} />
+              <Skeleton animation="wave" height={100} width={'100%'} />
+            </>) :
+            (
+              <List className={classes.list}>
+                {expenses.map(expense => (
+                  <ListItem key={expense.id}>
+                    <Card className={classes.card}>
+                      <CardHeader title={expense.name}
+                        action={
+                          <>
+                            <Typography>
+                              {expense.date}
+                            </Typography>
+                            <Typography color='textSecondary'>
+                              <NumberFormat
+                                value={expense.cost}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                                prefix={'$'}
+                                renderText={formatedValue => formatedValue}
+                              />
+                            </Typography>
+                          </>
+                        }>
+                      </CardHeader>
+                      <CardContent className={classes.cardContent}>
+                        <Typography variant='body1'>
+                          <strong>Descripcion:</strong> {expense.description}
+                        </Typography>
+                        <Typography variant='body1'>
+                          <strong>Categoria:</strong> {expense.category}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </ListItem>
+                ))}
+              </List>
+            )
+          }
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* <button onClick={() => dispatch(putExpense(modifiedExpenses(expenses)))}>add Expense</button> */}
     </div>
   );
 }
